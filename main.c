ce2234d0b9f1211368480c1ad75582abb12288a8
dylooz <57732320+Dylooz@users.noreply.github.com>
2020-12-22 00:53:40 +0000

    commit

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 #include "vec2d.h"
 
-#define SCREEN_TITLE "Raycasting in C with SDL"
+#define WINDOW_TITLE "Raycasting in C with SDL"
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 800
 
@@ -18,12 +18,24 @@ int main(int argc, char **argv) {
 	 	SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         return 1;
     } else if (SDL_WasInit(SDL_INIT_EVERYTHING) == 0) {
-    	if ((window = SDL_CreateWindow(SCREEN_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN)) == NULL) {
-    		SDL_Log("Unable to create SDL_Window: %s", SDL_GetError());
+    	if ((window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN)) == NULL) {
+    		SDL_Log("Unable to create Window: %s", SDL_GetError());
+    		SDL_Quit();
     		return 2;
+    	} else {
+    		if ((screenSurface = SDL_GetWindowSurface(window)) == NULL) {
+    			SDL_Log("Unable to fetch Window Surface: %s", SDL_GetError());
+    			SDL_DestroyWindow(window);
+    			SDL_Quit();
+    			return 3;
+    		}
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+            SDL_UpdateWindowSurface(window);
+
     	}
-    	sleep(5);
     }
+
+    SDL_DestroyWindow(window);
 
     SDL_Quit();
 
